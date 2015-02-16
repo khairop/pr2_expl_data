522e89fa010920a8c25e2b0bf30de38e9c0ea1fa
Kostas Chairopoulos <khairop@gmail.com>
2015-02-16 18:33:51 +0200

    third commit

---
+++ plot2.R
@@ -0,0 +1,26 @@
+library(plyr)
+
+# load data into R
+nei = readRDS("./data/summarySCC_PM25.rds")
+scc = readRDS("./data/Source_Classification_Code.rds")
+
+# task 1 Have total emissions from PM2.5 decreased in the United States
+# from 1999 to 2008? Using the base plotting system, make a plot showing
+# the total PM2.5 emission from all sources for each of the years 1999, 
+# 2002, 2005, and 2008.
+
+#filter out the baltimore City, Maryland
+nei=subset(nei,nei$fips=="24510")
+
+# calculate total pm2.5 emissions for each year using ddply function
+#the . in ddply is used for direct column naming
+neisum <- ddply(nei, .(year), summarise, sumem = sum(Emissions))
+
+# creating plots
+png("plot2.png", width = 480, height = 480)
+
+plot(neisum$year, neisum$sumem, xlab="Year", ylab = "Total PM2.5 Emissions", pch = 19, col = "blue")
+
+title(main = "Total PM2.5 Emissions Per Year In Baltimore City")
+
+dev.off()
